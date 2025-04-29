33599f34e672ad24a719c4d419b7071eb89498d7
Josh Dicker <joshdicker@mac.mynetworksettings.com>
2025-04-29 11:29:50 -0400

    Initial commit - Hummer app setup

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <header>
+          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
+          </header>
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default App
